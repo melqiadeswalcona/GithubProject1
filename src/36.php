2916dac2a16f55e96cb48036dfbc0ac413346fe3
melqiadeswalcona <melqiadeswalcona@gmail.com>
2025-05-18 18:49:47 +0000

    Add 36.php

---
+++ src/36.php
@@ -0,0 +1,11 @@
+<?php
+// Define some variables
+$variable1 = 5;
+$variable2 = "Hello";
+
+// Perform some operations with the variables
+$result = $variable1 + $variable2;
+
+// Output the result
+echo "The sum of $variable1 and $variable2 is: $result";
+?>
